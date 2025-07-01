6657b0a3291de1e8f3b3a6c03bd7bd595170d927
Diego Tapia Canaval <dtapia309@gmail.com>
2025-06-30 23:52:45 -0400

    Delete entrega_2 directory

--- entrega_2/servicios/procesador/run_pipeline.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Creando directorios de resultados..."
-mkdir -p /root/results/comuna /root/results/tipo /root/results/tiempo
-
-echo "Ejecutando análisis con Python..."
-python /root/analisis_python.py
-
-echo "Proceso completado correctamente."
